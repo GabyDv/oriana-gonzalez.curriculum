edcf7f9e5fbb62d687d484c16ad4650ec0ab902b
GabyDv <gabriel.mansilla@davinci.edu.ar>
2025-09-22 00:50:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Scroll suave al presionar el botón
+document.getElementById('scrollBtn').addEventListener('click', () => {
+    document.getElementById('about').scrollIntoView({behavior: 'smooth'});
+  });
+  
